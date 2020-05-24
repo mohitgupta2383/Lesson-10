78ae7541e8757802ff9133f52f5ad501b5a35a1b
Mohit Gupta <mohit.gupta@service.nsw.gov.au>
2020-05-24 13:49:19 +1000

    updated

--- src/ConsoleApp.js
+++ src/ConsoleApp.js
@@ -1,8 +1,8 @@
 const readline = require('readline');
 const path = require("path");
-const { StudentDataReader ,TeacherDataReader } = require("./DataLayer");
-const { Student } = require("./Models")
-
+const { StudentDataReader, TeacherDataReader } = require("./DataLayer");
+const { StudentService } = require("./Services");
+const { Student, Teacher } = require("./Models");
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -21,40 +21,63 @@ function askQuestion(question) {
 
 
 async function Program() {
-    const baseFilePath = path.join(__dirname, "../","JSONData");
+    const baseFilePath = path.join(__dirname, "../", "JSONData");
     const _studentDataReader = new StudentDataReader(path.join(baseFilePath, "Students.json"));
-    const _taecherDataReader = new TeacherDataReader(path.join(baseFilePath, "teachers.json"));
+    const _teacherDataReader = new TeacherDataReader(path.join(baseFilePath, "Teachers.json"));
+    const _studentService = new StudentService(_studentDataReader, _teacherDataReader);
 
-    console.log(_studentDataReader.getArrayFromFile());
-    console.log(_taecherDataReader.getArrayFromFile());
+    // console.log(_studentDataReader.getArrayFromFile());
+    // console.log(_teacherDataReader.getArrayFromFile());
 
     let shouldLoop = true;
     while (shouldLoop) {
         console.log("[1] Students");
         console.log("[2] Teachers");
         console.log("[3] Exit");
-        let userInput = await askQuestion("Select an option from above; ");
+        let userInput = await askQuestion("Select an option from above: ");
         switch (userInput) {
             case "1":
                 console.log("[1] Add Student");
-                console.log("[2] Seatrch For Student");
-                console.log("[3] Upadte Student");
-                console.log("[4] Delete Student");
-                console.log("[5] Go Back");
-                let userInputStudent = await askQuestion ("Select an option from above;");
-                switch (userInputStudent){
+                console.log("[2] Search Students");
+                console.log("[3] Search For Student");
+                console.log("[4] Update Student");
+                console.log("[5] Delete Student");
+                console.log("[6] Go Back");
+                let userInputStudent = await askQuestion("Select an option from above: ");
+                switch (userInputStudent) {
                     case "1":
-                        let studentFirstName = await askQuestion("Enter Student First Name; ");
-                        let studentLastName = await askQuestion ("Enter Student Last Name; ");
-                        let studentAge = await askQuestion("Enter Student age; ");
-                        // let parseStudentAge = parseint(studentAge);
-                        let grades = await askQuestion ("Enter Student Grades (Space-Separated);");
-                        let teacherID = await askQuestion ("Enter teacher ID; ");
-                        let parseGrades = grades.split(" ")
-                        let newStudent = new Student(studentFirstName, studentLastName, studentAge, parseGrades, teacherID)
-                        _studentDataReader.addStudent(newStudent)
-
+                        let studentFirstName = await askQuestion("Enter Student First Name: ");
+                        let studentLastName = await askQuestion("Enter Student Last Name: ");
+                        let studentAge = await askQuestion("Enter Student Age: ");
+                        let parsedStudentAge = parseInt(studentAge);
+                        let grades = await askQuestion("Enter Student Grades (Space-Separated): ");
+                        let teacherId = await askQuestion("Enter Teacher's ID: ");
+                        let parsedGrades = grades.split(" ").map(num => parseInt(num));
+                        let newStudent = new Student(
+                            studentFirstName,
+                            studentLastName,
+                            parsedStudentAge,
+                            parsedGrades,
+                            teacherId
+                        );
+                        _studentService.addStudent(newStudent);
+                        break;
+                    case "2":
+                        let searchTerm = await askQuestion("Enter search term: ");
+                        let matchingStudents = _studentService.searchByName(searchTerm);
+                        console.log(matchingStudents);
+                        break;
+                    default:
+                        console.log("Going back to main menu");
                 }
+                break;
+            case "2":
+                break;
+            case "3":
+                shouldLoop = false;
+                break;
+            default:
+                console.log("Error: Could not read user input. Please enter a number from 1 to 3");
         }
     }
 }
